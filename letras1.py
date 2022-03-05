5645e664fe794406fa50ca2e652de3c513018a2d
Diomer2021 <85360619+Diomer2021@users.noreply.github.com>
2022-03-05 11:45:06 -0500

    letras comprension

--- letras1.py
+++ letras1.py
@@ -1,7 +1,4 @@
-letras = []
-for i in 'Python es el mejor lenguaje de programación':
-    letras.append(i)
-    
+letras = [i for i in 'Python es el mejor lenguaje de programación' if i != 'm']
 print(letras)
 
 
